56084551299b9794f721abb9406ee53549f8b737
MuhammadUmar-coder <muhammadumar.codes@gmail.com>
2025-08-05 17:35:27 +0500

    commit

--- OOPS_IN_C++/UsingHeap.cpp
+++ OOPS_IN_C++/UsingHeap.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 using namespace std;
+class student
+{
+private:
+    string names;
+    int rollno;
+    double *ptr;
 
-int main(){
+public:
+    student(string names, int rollno, float cgpa)
+    {
+        this->names = names;
+        this->rollno = rollno;
+        ptr = new double;
+        *ptr = cgpa;
+    }
+    void show()
+    {
+        cout << "The Names  is" << names << endl;
+        cout << "The Rollno  is" << rollno << endl;
+        cout << "The CGPA   is" << *ptr << endl;
+    }
+};
+int main()
+{
+    student objOne("umarkhan", 34, 3.3);
+    objOne.show();
 
-
-
-
-    return  0;
+    return 0;
 }
